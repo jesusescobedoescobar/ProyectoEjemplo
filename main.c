de71adb22a998ec66fc129c62e301c4bd73bb06a
jesusescobedoescobar <jesusescobedo.23@gmail.com>
2026-06-10 16:22:56 -0600

    Arreglar el issue

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
 int main (void){
 	printf("Hola mundo");
+	//git clone <repo>
+	//git fetch
+	//git pull origin main
+	//git commit -am "mensaje"
+	//git push origin main
+	printf("Cerrar ventana");
 }
